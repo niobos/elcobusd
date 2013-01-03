acdc49067dfe99b65825fd600a4ae8382cad2bd9
Niels Laukens <niels.laukens@vrt.be>
2013-01-03 15:50:22 +0100

    Added return temperature

--- rrdtool/create-BT-db.sh
+++ rrdtool/create-BT-db.sh
@@ -10,5 +10,6 @@ fi
 rrdtool create "$ID-BT.rrd" --no-overwrite --step 60 --start "-8 years" \
 	"DS:temperature:GAUGE:120:0:100" \
 	"DS:set_temperature:GAUGE:120:0:100" \
+	"DS:return_temperature:GAUGE:120:0:100" \
 	"RRA:AVERAGE:0.5:1:$(( 60*24*365*2 ))" \
 	"RRA:AVERAGE:0.5:10:$(( 6*24*365*10 ))"
--- rrdtool/update-BT.sh
+++ rrdtool/update-BT.sh
@@ -25,6 +25,8 @@ DATA="$(
 		print "\xdc\x81\x00\x0b\x06\x3d\x0d\x05\x19\x99\x23";
 		sleep 1;
 		print "\xdc\x81\x00\x0b\x06\x3d\x0d\x09\x23\x4b\x57";
+		sleep 1;
+		print "\xdc\x81\x00\x0b\x06\x3d\x11\x05\x1a\x9f\x42";
 		sleep 2;
 		' |
 	socat - TCP6:[::1]:8446 | $BINPATH/elcobus-log-bus.pl | $BINPATH/elcobus-parse-log.pl
@@ -33,7 +35,8 @@ DATA="$(
 
 TEMP="$(   echo "$DATA" | grep " : BoilerTemp:" | tail -n1 | sed 's/.*Temp=\([0-9.]*\)C.*/\1/' )"
 TTEMP="$(  echo "$DATA" | grep " : BoilerSetTemp:" | tail -n1 | sed 's/.*Temp=\([0-9.]*\)C.*/\1/' )"
+RTEMP="$(  echo "$DATA" | grep " : ReturnTemp:" | tail -n1 | sed 's/.*Temp=\([0-9.]*\)C.*/\1/' )"
 
 rrdtool update "$FILENAME" --template \
-	"temperature:set_temperature" \
-	"N:${TEMP:-U}:${TTEMP:-U}"
+	"temperature:set_temperature:return_temperature" \
+	"N:${TEMP:-U}:${TTEMP:-U}:${RTEMP:-U}"
